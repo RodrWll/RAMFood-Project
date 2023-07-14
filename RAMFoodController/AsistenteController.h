ea367781c3c3780e50a7b91179acfa9d4d6237f6
Franco Galvez <a20192806@pucp.edu.pe>
2023-07-14 13:14:32 -0500

    Asistencia y Cobranza requerida listas

--- RAMFoodController/AsistenteController.h
+++ RAMFoodController/AsistenteController.h
@@ -4,18 +4,23 @@ namespace RAMFoodController {
 	using namespace System::Collections::Generic;
 	using namespace System;
 	using namespace RAMFoodModel;
+	using namespace System::Data::SqlClient;
 	public ref class AsistenteController{
+		private:
+			SqlConnection^ objConexion;
 		public:
 			AsistenteController();
-			/*Funcion generica para leer el archivo txt
-			* este archivo debe tener 2 datos, el id del plato y la cantidad pedida
-			* seran 2 funciones, una para bebidas y otra para comida
-			*/
+			void abrirConexion();
+			void cerrarConexion();
 			List<Plato^>^ listarPlatosPedidosMesa();
 			List<Bebida^>^ listarBebidasPedidosMesa();
-			String^ LeerArchivoEstadoAsistencia(int numeroMesa);
-			String^ LeerArchivoEstadoCobranza(int numeroMesa);
-			void ModificarEstadoAsistencia(int numeroDeMesaALeer, int estadoAsis);
-			void ModificarEstadoCobranza(int numeroDeMesaALeer, int estadoCob);
+			//String^ LeerArchivoEstadoAsistencia(int numeroMesa);
+			//String^ LeerArchivoEstadoCobranza(int numeroMesa);
+			/*void ModificarEstadoAsistencia(int numeroDeMesaALeer, int estadoAsis);*/
+			//void ModificarEstadoCobranza(int numeroDeMesaALeer, int estadoCob);
+			int BuscarEstadoAsistencia(int numeroMesa);
+			int BuscarEstadoCobranza(int numeroMesa);
+			void ActualizarEstadoAsistencia(int numeroMesa, int estadoAsis);
+			void ActualizarEstadoCobranza(int numeroMesa, int estadoCob);
 	};
 }
